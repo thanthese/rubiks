5eb381dbc38ccc6647ab6ff86664cd844ca47fd0
Stephen Mann <thanthese@gmail.com>
2012-03-04 13:06:17 -0600

    switched to using records

--- src/rubiks/core.clj
+++ src/rubiks/core.clj
@@ -15,6 +15,8 @@
   [color]
   (vec (repeat 9 color)))
 
+(defrecord Cube [back bottom front left right top])  ; note alphabetical order
+
 (def solved-cube
   "
   The order works thusly.  For the :front face:
@@ -33,32 +35,39 @@
   For the :bottom face, same trick only this time you're pouring the coffee
   away from you.
   "
-  {:top (solid-face :white)
-   :front (solid-face :orange)
-   :left (solid-face :blue)
-   :right (solid-face :green)
-   :back (solid-face :red)
-   :bottom (solid-face :yellow)})
+  (Cube. (solid-face :red)     ; back
+         (solid-face :yellow)  ; bottom
+         (solid-face :orange)  ; front
+         (solid-face :blue)    ; left
+         (solid-face :green)   ; right
+         (solid-face :white))) ; top
 
 (defn R
   "Turn right face clockwise."
   [{:keys [top front left right back bottom] :as cube}]
-  {:left left
-   :top [(top 0) (top 1) (front 2)
-         (top 3) (top 4) (front 5)
-         (top 6) (top 7) (front 8)]
-   :right [(right 6) (right 3) (right 0)
-           (right 7) (right 4) (right 1)
-           (right 8) (right 5) (right 2)]
-   :back [(top 8) (back 1) (back 2)
-          (top 5) (back 4) (back 5)
-          (top 2) (back 7) (back 8)]
-   :bottom [(bottom 0) (bottom 1) (back 6)
-            (bottom 3) (bottom 4) (back 3)
-            (bottom 6) (bottom 7) (back 0)]
-   :front [(front 0) (front 1) (bottom 2)
-           (front 3) (front 4) (bottom 5)
-           (front 6) (front 7) (bottom 8)]})
+  (Cube.
+    ; back
+    [(top 8) (back 1) (back 2)
+     (top 5) (back 4) (back 5)
+     (top 2) (back 7) (back 8)]
+    ; bottom
+    [(bottom 0) (bottom 1) (back 6)
+     (bottom 3) (bottom 4) (back 3)
+     (bottom 6) (bottom 7) (back 0)]
+    ; front
+    [(front 0) (front 1) (bottom 2)
+     (front 3) (front 4) (bottom 5)
+     (front 6) (front 7) (bottom 8)]
+    ; left
+    left
+    ; right
+    [(right 6) (right 3) (right 0)
+     (right 7) (right 4) (right 1)
+     (right 8) (right 5) (right 2)]
+    ; top
+    [(top 0) (top 1) (front 2)
+     (top 3) (top 4) (front 5)
+     (top 6) (top 7) (front 8)]))
 
 (def R2 (comp R R))
 (def R3 (comp R R R))
@@ -66,22 +75,29 @@
 (defn U
   "Turn top face clockwise."
   [{:keys [top front left right back bottom] :as cube}]
-  {:bottom bottom
-   :top [(top 6) (top 3) (top 0)
-         (top 7) (top 4) (top 1)
-         (top 8) (top 5) (top 2)]
-   :front [(right 0) (right 1) (right 2)
-           (front 3) (front 4) (front 5)
-           (front 6) (front 7) (front 8)]
-   :left [(front 0) (front 1) (front 2)
-          (left 3) (left 4) (left 5)
-          (left 6) (left 7) (left 8)]
-   :right [(back 0) (back 1) (back 2)
-           (right 3) (right 4) (right 5)
-           (right 6) (right 7) (right 8)]
-   :back [(left 0) (left 1) (left 2)
-          (back 3) (back 4) (back 5)
-          (back 6) (back 7) (back 8)]})
+  (Cube.
+    ; back
+    [(left 0) (left 1) (left 2)
+     (back 3) (back 4) (back 5)
+     (back 6) (back 7) (back 8)]
+    ; bottom
+    bottom
+    ; front
+    [(right 0) (right 1) (right 2)
+     (front 3) (front 4) (front 5)
+     (front 6) (front 7) (front 8)]
+    ; left
+    [(front 0) (front 1) (front 2)
+     (left 3) (left 4) (left 5)
+     (left 6) (left 7) (left 8)]
+    ; right
+    [(back 0) (back 1) (back 2)
+     (right 3) (right 4) (right 5)
+     (right 6) (right 7) (right 8)]
+    ; top
+    [(top 6) (top 3) (top 0)
+     (top 7) (top 4) (top 1)
+     (top 8) (top 5) (top 2)]))
 
 (def U2 (comp U U))
 (def U3 (comp U U U))
@@ -91,64 +107,59 @@
           cube
           moves))
 
-;;;  threads/solver  ;;;
+;;;  solver  ;;;
 
 (defn in? [ls n] (some (partial = n) ls))
 (def not-in? (comp not in?))
 
-(defn construct-thread [cube]
-  {:cube cube
-   :move-history []})
+(defrecord History [cube move-history])
 
 (defn all-future-threads
   "Return a set of all possible future threads (one step in advance only)."
   [{:keys [cube move-history] :as thread}]
   (let [last-move (peek move-history)]
     (cond (or (= last-move :R) (= last-move :R2) (= last-move :R3))
-          #{{:cube (U cube) , :move-history (conj move-history :U)}
-            {:cube (U2 cube) , :move-history (conj move-history :U2)}
-            {:cube (U3 cube) , :move-history (conj move-history :U3)}}
+          #{(History. (U cube) (conj move-history :U))
+            (History. (U2 cube) (conj move-history :U2))
+            (History. (U3 cube) (conj move-history :U3))}
           ,,
           (or (= last-move :U) (= last-move :U2) (= last-move :U3))
-          #{{:cube (R cube) , :move-history (conj move-history :R)}
-            {:cube (R2 cube) , :move-history (conj move-history :R2)}
-            {:cube (R3 cube) , :move-history (conj move-history :R3)}}
+          #{(History. (R cube) (conj move-history :R))
+            (History. (R2 cube) (conj move-history :R2))
+            (History. (R3 cube) (conj move-history :R3))}
           ,,
           :else
-          #{{:cube (R cube) , :move-history (conj move-history :R)}
-            {:cube (R2 cube) , :move-history (conj move-history :R2)}
-            {:cube (R3 cube) , :move-history (conj move-history :R3)}
-            {:cube (U cube) , :move-history (conj move-history :U)}
-            {:cube (U2 cube) , :move-history (conj move-history :U2)}
-            {:cube (U3 cube) , :move-history (conj move-history :U3)}})))
+          #{(History. (R cube) (conj move-history :R))
+            (History. (R2 cube) (conj move-history :R2))
+            (History. (R3 cube) (conj move-history :R3))
+            (History. (U cube) (conj move-history :U))
+            (History. (U2 cube) (conj move-history :U2))
+            (History. (U3 cube) (conj move-history :U3))})))
 
 (defn fail-msg-max-attempts "Failure: max attempts reached.")
 
 (defn solve [initial-state max-depth]
   (loop [attempts 1
          tried-states [initial-state]
-         active-threads [(construct-thread initial-state)]]
+         active-threads [(History. initial-state [])]]
     (let [all-future-threads (mapcat all-future-threads active-threads)
-          novel-threads all-future-threads
-          ;(filter (fn [thread]
-          ;                        (not-in? tried-states (:cube thread)))
-          ;                      all-future-threads)
           solved-threads (filter (fn [thread]
                                    (= solved-cube (:cube thread)))
-                                 novel-threads)]
-      (println (format "Step %s: novel threads: %s, all-future-threads %s, ratio %s"
+                                 all-future-threads)]
+      (println (format "Step %s:  number of threads %s"
                        attempts
-                       (count novel-threads)
-                       (count all-future-threads)
-                       (/ (count novel-threads) (count all-future-threads))))
-      (if (not (empty? solved-threads))
-        (map :move-history solved-threads)
-        (if (>= attempts max-depth)
-          fail-msg-max-attempts
-          (recur
-            (inc attempts)
-            (concat tried-states (map :cube novel-threads))
-            novel-threads))))))
+                       (count all-future-threads)))
+      (cond (not (empty? solved-threads))
+            (map :move-history solved-threads)
+            ,,
+            (>= attempts max-depth)
+            fail-msg-max-attempts
+            ,,
+            :else
+            (recur
+              (inc attempts)
+              (concat tried-states (map :cube all-future-threads))
+              all-future-threads)))))
 
 ;;;  tests  ;;;
 
@@ -183,12 +194,12 @@
 (deftest
   gibberish-move
   (is (= (move solved-cube R U R U2 R3 U3 R)
-         {:top [w w w o w w y y w]
-          :front [g g b o o r o o r]
-          :bottom [y y w y y w y y y]
-          :left [b g r b b b b b b]
-          :right [o o o g g b g g o]
-          :back [g r r w r r g r r]})))
+         (Cube. [g r r w r r g r r]
+                [y y w y y w y y y]
+                [g g b o o r o o r]
+                [b g r b b b b b b]
+                [o o o g g b g g o]
+                [w w w o w w y y w]))))
 
 (deftest
   test-in?
@@ -231,19 +242,23 @@
 ; 6 deep
 (= [[:U2 :R3 :U3 :R :U3 :R3]]
          (time (solve (move solved-cube R U R3 U R U2) 6)))
-; 4,438 ms: no optimizations
-; 2,871 ms: ratio of 1 (no accidental back-tracking)
-;    72 ms: don't check that states are unique
+; 1.  4,438 ms:  no optimizations
+; 2.  2,871 ms:  ratio of 1 (no accidental back-tracking)
+; 3.     72 ms:  don't check that states are unique
+; 4.     63 ms:  moved to using records for Cubes and History
 
 ; reverse sune (8 deep)
 (= [[:U3 :R :U2 :R3 :U3 :R :U3 :R3]]
          (time (solve (move solved-cube R U R3 U R U2 R3 U) 8)))
-; 356,186 ms: no optimization
-; 235,637 ms: ratio of 1 (no accidental back-tracking)
-;     435 ms: don't check that states are unique
+; 1.  356,186 ms
+; 2.  235,637 ms
+; 3.      435 ms
+; 4.      377 ms
 
 ; 10 deep
 (= [[:U :R :U3 :R :U2 :R3 :U3 :R :U3 :R3]]
          (time (solve (move solved-cube R U R3 U R U2 R3 U R3 U3) 10)))
+; 3.      out of memory
+; 4.      out of memory
 
 )
